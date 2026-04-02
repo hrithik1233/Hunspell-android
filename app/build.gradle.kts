9b03d49f0223f3990275ec9a89215cfb86a6bc3f
Hrithik <hridikhh@gmail.com>
2026-04-02 12:25:12 +0530

    new

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -25,7 +25,7 @@ android {
             )
         }
 
-        minSdk = 24
+        minSdk = 26
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
@@ -54,7 +54,7 @@ android {
 
 
     buildFeatures {
-        compose = true
+        compose = false
     }
 
     composeOptions {
@@ -69,16 +69,7 @@ android {
     }
 }
 
-dependencies {
-    implementation("androidx.core:core-ktx:1.13.0")
-    implementation("androidx.activity:activity-compose:1.9.0")
-    implementation("androidx.compose.ui:ui:1.6.0")
-    implementation("androidx.compose.material3:material3:1.2.0")
-    implementation("androidx.compose.ui:ui-tooling-preview:1.6.0")
-    debugImplementation("androidx.compose.ui:ui-tooling:1.6.0")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
-    implementation("androidx.compose.runtime:runtime-livedata:1.6.0")
-}
+
 
 afterEvaluate {
     publishing {
